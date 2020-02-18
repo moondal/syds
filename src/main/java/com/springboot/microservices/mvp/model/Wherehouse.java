e6e8a4f5c78c15e0d56375430b930ab30fa77384
minsuhan <minsu.han@samyang.com>
2020-02-18 16:54:38 +0900

    commit

---
+++ src/main/java/com/springboot/microservices/mvp/model/Wherehouse.java
@@ -0,0 +1,14 @@
+package com.springboot.microservices.mvp.model;
+
+import lombok.Data;
+
+@Data
+public class Wherehouse {
+	private String barcode;
+	private String itemCd;
+	private String itemNm;
+	private int qty;
+	private int capaQty;
+	private String useYn;
+	private String locNm;
+}
--- src/main/java/com/springboot/microservices/mvp/rest/StockController.java
+++ src/main/java/com/springboot/microservices/mvp/rest/StockController.java
@@ -1,7 +1,6 @@
 package com.springboot.microservices.mvp.rest;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,14 +9,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.multipart.MultipartFile;
-
 
 import com.springboot.microservices.mvp.dao.StockDao;
 import com.springboot.microservices.mvp.model.Stock;
+import com.springboot.microservices.mvp.model.Wherehouse;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -70,16 +66,15 @@ public class StockController {
 		return new ResponseEntity<String> (re+"", HttpStatus.OK);
 	}
 	
-	@ApiOperation(value="재고 감소(param:locCd,barcode)")
-	@RequestMapping(value="/setstockdelete", method=RequestMethod.POST)
-	public ResponseEntity <String> setStockDelete(
-			@RequestBody Stock stock
+	@ApiOperation(value="재고현황조회")
+	@RequestMapping(value="/getallstocks", method=RequestMethod.GET)
+	public ResponseEntity  <List<Wherehouse>> setStockDelete(
 		) throws Exception { 
 		
 		log.info("Start db update");
-		int re  = stockDao.updateStockDelete(stock);
+		List<Wherehouse> re  = stockDao.selectWherehouseList();
 		log.debug("result :"+ re);
 		
-		return new ResponseEntity<String> (re+"", HttpStatus.OK);
+		return new ResponseEntity<List<Wherehouse>> (re, HttpStatus.OK);
 	}
 }
